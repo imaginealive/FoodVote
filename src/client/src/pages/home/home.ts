99e7e8c4c729364e37d81484fc23d26a10c652a9
Perawatt <pekung4433@perfenterprise.com>
2018-09-01 12:00:46 +0700

    login

--- src/client/src/pages/home/home.ts
+++ src/client/src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {
+  FormGroup,
+  FormControl
+
+} from '@angular/forms';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  langs;
+  langForm;
 
   constructor(public navCtrl: NavController) {
-
+    this.langForm = new FormGroup({
+      "langs": new FormControl({value: 'rust', disabled: false})
+    });
+  }
+  doSubmit(event) {
+    console.log('Submitting form', this.langForm.value);
+    event.preventDefault();
   }
 
 }
